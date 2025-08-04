28aa1263ab2cd951a49372254c4ca31e6d6b52fa
minjonyyy <minjonyyy@naver.com>
2025-08-04 15:49:06 +0900

    fix: ESLint 설정 문제 해결
    - 루트 디렉토리에 eslint.config.js 추가 (CommonJS 형식)
    - Node.js 전역 변수 설정 추가
    - pre-commit 훅이 정상 작동하도록 설정
    - ESLint v9 호환성 문제 해결

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        process: "readonly",
+        console: "readonly",
+        Buffer: "readonly",
+        global: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
